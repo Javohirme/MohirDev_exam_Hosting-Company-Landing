fbd5813b979a91a7d8a30ffc50ecd583bb57952b
JavohirToshov <jt997049811@gmail.com>
2025-07-23 14:03:38 +0500

    Nav add 80%

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let header__menu = document.querySelector(".header__menu");
-
-function addOpen() {
-  header__menu.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
